02f4bfd79f55985a72c7cddbbd42273ebd30b9b6
Aleksey Kladov <aleksey.kladov@gmail.com>
2014-05-10 20:18:25 +0400

    work in work

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@ FIELS="diploma.tex diploma.bib matmex-diploma-custom.cls"
 cp $FIELS img/* res/* tmp
 cd tmp
 xelatex diploma.tex
-mv diploma.pdf ..
+xelatex diploma.tex
+cd ..
+mv tmp/diploma.pdf .
+xdg-open diploma.pdf
